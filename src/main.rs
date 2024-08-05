f04544bf51c5d5ffc293a4e5d2545d249070f88d
ishak <ishaksebsib@gmail.com>
2024-08-05 18:43:19 +0300

    [ENH] print server url

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,6 @@ use newsletter::run;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let address = TcpListener::bind("127.0.0.1:8000")?;
+    println!("Listening on http://{}", address.local_addr()?);
     run(address)?.await
 }
