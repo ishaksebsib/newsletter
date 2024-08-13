6e1e4e1b55391a56abf7eef4a227e47e21dbc811
ishak <ishaksebsib@gmail.com>
2024-08-13 18:00:34 +0300

    [ENH] add health_check api and integration test for it

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -34,7 +34,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
-        .post(&format!("{}/subscribe", &app_address))
+        .post(&format!("{}/subscriptions", &app_address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
