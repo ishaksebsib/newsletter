5571f0fb0efb90424cf48b51e31fa5bc85508928
ishaksebsib <ishaksebsib@gmail.com>
2026-01-08 14:43:03 +0300

    feat: add logger

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use std::net::TcpListener;
 
+use env_logger::Env;
 use newsletter::startup::run;
 use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let config = newsletter::config::get_configuration().expect("Failed to load configuration");
 
     let address = format!("127.0.0.1:{}", config.application_port);
     let listener = TcpListener::bind(address)?;
-    println!("Listening on http://{}", listener.local_addr()?);
 
     let db_pool = PgPool::connect(&config.database.connection_string())
         .await
