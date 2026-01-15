4489d387bffa9536b40702a91d387ef4194b1a6b
ishaksebsib <ishaksebsib@gmail.com>
2026-01-15 20:27:08 +0300

    feat: add tracing

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
-pub mod routes;
 pub mod config;
+pub mod routes;
 pub mod startup;
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,6 +13,16 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!("Adding a new subscriber.",
+        %request_id,subscriber_email = %form.email,subscriber_name = %form.name);
+    let _request_span_guard = request_span.enter();
+    log::info!(
+        "Adding '{}' '{}' as a new subscriber.",
+        form.email,
+        form.name
+    );
+    log::info!("Saving new subscriber details in the database");
     match query!(
         r#"
 INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -26,9 +36,12 @@ VALUES ($1, $2, $3, $4)
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!("New subscriber details have been saved");
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::error!("Failed to save new subscriber details in the database: {e}");
             HttpResponse::InternalServerError().finish()
         }
     }
